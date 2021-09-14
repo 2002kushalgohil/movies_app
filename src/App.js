6f1f8cf6d978ff93fb888d8b4fb441a09cb06322
kushal gohil <2002kushalgohil@gmail.com>
2021-09-14 14:18:35 +0530

    Completed the App module

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import notFound from "./assets/notFound.gif";
+import gif1 from "./assets/404gif1.gif";
+import gif2 from "./assets/404gif2.gif";
 function App() {
   const featured = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5382c539ec8faa8eab75274e3030f3f2`;
   const images = `https://image.tmdb.org/t/p/w1280`;
@@ -30,34 +33,111 @@ function App() {
   useEffect(() => {
     getData(featured);
   }, []);
-  return (
-    <>
-      <div id="navBar">
-        <input type="text" value={userInput} onChange={inputHandler} />
-        <button onClick={submitHandler}>Go</button>
-      </div>
-      <div id="home">
-        <div id="cardContainer">
-          {userData.map((value) => {
-            return (
-              <div key={value.id} className="card">
-                <div className="cardImage">
-                  <img src={images + value.poster_path} />
-                </div>
-                <div className="cardInfo">
-                  <h1>{value.title}</h1>
-                  <h1>{value.original_language}</h1>
-                  <h1>{value.overview}</h1>
-                  <h1>{value.release_date}</h1>
-                  <h1>{value.vote_average}</h1>
-                </div>
-              </div>
-            );
-          })}
+  if (userData.length === 0) {
+    return (
+      <>
+        <div id="navBar">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <input
+              type="text"
+              value={userInput}
+              onChange={inputHandler}
+              placeholder="Search Movie"
+            />
+
+            <button type="submit" onClick={submitHandler}>
+              Go
+            </button>
+          </form>
         </div>
-      </div>
-    </>
-  );
+        <div id="home">
+          <div className="notFound">
+            <img className="not gif1" src={gif1} alt="images" />
+            <h1 className="notFoundHeading">404 Not Found</h1>
+            <img className="not gif2" src={gif2} alt="images" />
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div id="navBar">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <input
+              type="text"
+              value={userInput}
+              onChange={inputHandler}
+              placeholder="Search Movie"
+            />
+
+            <button type="submit" onClick={submitHandler}>
+              Go
+            </button>
+          </form>
+        </div>
+        <div id="home">
+          <div id="cardContainer">
+            {userData.map((value) => {
+              const {
+                id,
+                overview,
+                release_date,
+                poster_path,
+                title,
+                vote_average,
+              } = value;
+              return (
+                <div key={id} className="card">
+                  <div className="cardHiddenInfo">
+                    <div>
+                      <p className="cardHiddenInfoHeading">Overview:- </p>
+                      <p>{overview} </p>
+                    </div>
+
+                    <div>
+                      <p className="cardHiddenInfoHeading">Release Date:- </p>
+                      <p>{release_date} </p>
+                    </div>
+                  </div>
+
+                  <div className="cardImage">
+                    <img
+                      alt="images"
+                      src={
+                        poster_path != null ? images + poster_path : notFound
+                      }
+                    />
+                  </div>
+
+                  <div className="cardInfo">
+                    <p className="cardInfoTitle">{title}</p>
+                    <p
+                      className={
+                        vote_average < 5
+                          ? "cardInfoVote red"
+                          : "cardInfoVote green"
+                      }
+                    >
+                      {vote_average}
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
